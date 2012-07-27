202e046f00064bce0cbedbeec52be95bedd20dc2
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-07-27 19:13:18 -0300

    lib: attach external callback and check for black screens

--- lib.py
+++ lib.py
@@ -1,16 +1,44 @@
+import sys
+sys.path.insert(0, "/Library/Frameworks/GStreamer.framework/Versions/0.10/x86_64/lib/python2.7/site-packages/gst-0.10")
+import gst
 
 class Detector(object):
-    def __init__(self, rtmp_uri):
+    def __init__(self, rtmp_uri, callback):
         self.rtmp_uri = rtmp_uri
+        self.external_callback = callback
 
-    def start(self, callback):
+        self.create_pipeline()
+        self.get_bus_and_link_callback()
+
+    def start(self):
         '''
         when it starts, Detector should start a
         connect-check-disconnect cycle on RTMP
         stream looking for black screens.
         '''
-        self.callback = callback
         self.connect_and_check_for_blackscreen()
 
+    def create_pipeline(self): #need tests
+        self.pipeline = gst.parse_launch("uridecodebin uri=%s !"
+                                         "videoanalyse name='analyse' !"
+                                         "ffmpegcolorspace !"
+                                         "autovideosink" % self.rtmp_uri)
+
+    def get_bus_and_link_callback(self): #need tests
+        self.bus = self.pipeline.get_bus()
+        self.bus.add_signal_watch()
+        self.bus.connect("message", self.internal_callback)
+
+    def internal_callback(self, bus, message): #need tests
+        if message.type == gst.MESSAGE_ELEMENT and \
+            message.structure.get_name() == "GstVideoAnalyse" and \
+            message.structure["brightness"] < 0.07:
+                self.external_callback()
+
+        elif message.type == gst.MESSAGE_ERROR:
+            err, debug = message.parse_error()
+            print "Error: %s" % err, debug
+
     def connect_and_check_for_blackscreen(self):
-        pass
+        self.pipeline.set_state(gst.STATE_PLAYING)
+
--- test_lib.py
+++ test_lib.py
@@ -7,22 +7,21 @@ def test_detector_should_call_callback_if_blackscreen_appears(monkeypatch):
     def callback():
         was_called.append(True)
 
-    detector = lib.Detector("rtmp://blackscreen/app/ins/stream")
-    detector.start(callback)
+    detector = lib.Detector("rtmp://blackscreen/app/ins/stream", callback)
+    detector.start()
 
     assert was_called
-
 
 def test_detector_should_not_call_callback_if_no_blackscreen_appears():
     was_called = []
     def callback():
         was_called.append(True)
 
-    detector = lib.Detector("rtmp://novela/app/ins/stream")
-    detector.start(callback)
+    detector = lib.Detector("rtmp://novela/app/ins/stream", callback)
+    detector.start()
 
     assert not was_called
 
 def fake_connect_and_check_for_blackscreen(self, *args):
     if 'blackscreen' in self.rtmp_uri:
-        self.callback()
+        self.external_callback()
