8a928922e82a742c07af01c5dd136493846b60a1
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-07-27 19:13:39 -0300

    main: use lib.Detector

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 import sys
-sys.path.insert(0, "/Library/Frameworks/GStreamer.framework/Versions/0.10/x86_64/lib/python2.7/site-packages/gst-0.10")
 sys.path.insert(0, "/Library/Frameworks/GStreamer.framework/Versions/0.10/x86_64/lib/python2.7/site-packages")
+import lib
 import glib, gobject
-import gst
 
-def callback(bus, message):
-    if message.type == gst.MESSAGE_ELEMENT:
-        if message.structure.get_name() == "GstVideoAnalyse" and message.structure['brightness'] < 0.07:
-            print "black screen."
-    elif message.type == gst.MESSAGE_ERROR:
-        err, debug = message.parse_error()
-        print "Error: %s" % err, debug
-        loop.quit()
+def callback():
+    print "black screen of death."
 
-pipeline = gst.parse_launch("uridecodebin uri=%s ! videoanalyse name='analyse' ! ffmpegcolorspace ! autovideosink" % sys.argv[1])
-bus = pipeline.get_bus()
-bus.add_signal_watch()
-bus.connect("message", callback)
-pipeline.set_state(gst.STATE_PLAYING)
+def start_all():
+    gobject.threads_init()
+    loop = glib.MainLoop()
+    loop.run()
 
-gobject.threads_init()
-loop = glib.MainLoop()
-loop.run()
-
-pipeline.set_state(gst.STATE_NULL)
-
+detector = lib.Detector(sys.argv[1], callback)
+detector.start()
+start_all()
 
